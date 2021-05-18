7ed08d3a97f781d84f2135d335eca0545df0c5e5
cclin <181250083@smail.nju.edu.cn>
2021-05-18 13:00:28 +0800

    init: react-image-gallery

---
+++ backend/server.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const app = express();
+const { cloudinary } = require("./utils/cloudinary");
+
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
+
+app.get("/api/images", async (req, res) => {
+  const { resources } = await cloudinary.search
+    .expression("folder: react_image_gallery")
+    .sort_by("public_id", "desc")
+    .max_results(30)
+    .execute();
+
+  const publicIds = resources.map((file) => file.public_id);
+  res.send(publicIds);
+});
+
+app.post("/api/upload", async (req, res) => {
+  try {
+    const fileStr = req.body.data;
+    const uploadedRes = await cloudinary.uploader.upload(fileStr, {
+      upload_preset: "react_image_gallery",
+    });
+    console.log(uploadedRes);
+    res.json({ msg: "Image uploaded successfully!" });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ err: "Something went wrong!" });
+  }
+});
+
+const port = process.env.PORT || 3001;
+app.listen(port, () => {
+  console.log(`listening on port ${port}`);
+});
